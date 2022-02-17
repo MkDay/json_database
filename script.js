cd2b673268d6de1383540465015fab8deabd53e8
MkDay <89559065+MkDay@users.noreply.github.com>
2022-02-17 20:54:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -31,6 +31,8 @@ let day = String(date.getDate()).padStart(2, '0');
 let chosenDate;
 let chosenMonth;
 
+const qualityList = ['sakko', 'ujucha', 'sujucha', 'suwachochassa', 'mudu'];
+
 const monthList = ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec'];
 
 const chartLabels = ['Day1', 'Day2', 'Day3', 'Day4', 'Day5', 'Day6', 'Day7', 'Day8', 'Day9', 'Day10', 'Day11', 'Day12', 'Day13', 'Day14', 'Day15', 'Day16', 'Day17', 'Day18', 'Day19', 'Day20', 'Day21', 'Day22', 'Day23', 'Day24', 'Day25', 'Day26', 'Day27', 'Day28', 'Day29', 'Day30', 'Day31'];
