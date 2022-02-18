79a20d768b335ef67abbc9a3c568fdf4e98f77ac
MkDay <89559065+MkDay@users.noreply.github.com>
2022-02-18 22:42:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 "use strict";
-const quality1 = document.querySelectorAll('.q1-score');
-const quality2 = document.querySelectorAll('.q2-score');
-const quality3 = document.querySelectorAll('.q3-score');
-const quality4 = document.querySelectorAll('.q4-score');
-const quality5 = document.querySelectorAll('.q5-score');
+const quality1 = document.querySelectorAll('.q1-days');
+const quality2 = document.querySelectorAll('.q2-days');
+const quality3 = document.querySelectorAll('.q3-days');
+const quality4 = document.querySelectorAll('.q4-days');
+const quality5 = document.querySelectorAll('.q5-days');
+
+const container1 = document.querySelector('#container1');
+const container2 = document.querySelector('#container2');
 
 const btn = document.querySelector('#submit');
 const test = document.querySelector('#test');
@@ -12,13 +15,6 @@ let scoreList = [[], [], [], [], []];
 
 let postList = [[], [], [], [], []];
 
-
-
-let scores1 = [];
-let scores2 = [];
-let scores3 = [];
-let scores4 = [];
-let scores5 = [];
 
 const today = document.querySelector('#today');
 const calendar = document.querySelector('#calendar');
@@ -30,19 +26,23 @@ let day = String(date.getDate()).padStart(2, '0');
 
 let chosenDate;
 let chosenMonth;
-
-const qualityList = ['sakko', 'ujucha', 'sujucha', 'suwachochassa', 'mudu'];
+let prevDate;
 
 const monthList = ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec'];
+
+const qualityList = ['sakko', 'ujucha', 'sujucha', 'suwachochassa', 'mudu'];
 
 const chartLabels = ['Day1', 'Day2', 'Day3', 'Day4', 'Day5', 'Day6', 'Day7', 'Day8', 'Day9', 'Day10', 'Day11', 'Day12', 'Day13', 'Day14', 'Day15', 'Day16', 'Day17', 'Day18', 'Day19', 'Day20', 'Day21', 'Day22', 'Day23', 'Day24', 'Day25', 'Day26', 'Day27', 'Day28', 'Day29', 'Day30', 'Day31'];
 
 
-/* ============= get month/date ================ */
+
+
+
+/* ============= load date & month ============== */
 
 window.addEventListener('load', (e) => {
  
- today.innerHTML += '<br>' + year + '/' + month + '/' + day;
+ today.innerHTML += year + '/' + month + '/' + day;
  calendar.value = String(year) + '-' + month + '-' + day;
  getChosenMonth();
  getChosenDate();
@@ -50,20 +50,35 @@ window.addEventListener('load', (e) => {
 });
 
 calendar.addEventListener('change', (e) => {
+  removePrev();
   getChosenMonth();
   getChosenDate();
 
 });
 
+const removePrev = () => {
+
+ for(let i=0; i<5; i++) {
+ 
+   let qDayClass = '.q' + (i+1) + '-days';
+    
+   for(let j=0; j<31; j++) { 
+       
+     if(prevDate === (j + 1)) {
+       document.querySelectorAll(qDayClass)[j].innerHTML = "";
+     }
+   }
+ }
+};
+
 
 const getChosenMonth = () => {
   if(calendar.value !== "") {
    chosenMonth = parseInt((calendar.value).slice(5, 7));
-   //test.textContent = parseInt(calendar.value.slice(5, 7)); 
+ 
     
   } else {
     chosenMonth = date.getMonth();
-    //test.textContent = date.getMonth();
   }
 };
 
@@ -72,12 +87,14 @@ const getChosenDate = () => {
 
  if(calendar.value !== "") {
    chosenDate = parseInt((calendar.value).slice(8));
-   //test.textContent = chosenDate;
+   
    
  } else {
    chosenDate = date.getDate();
-   //test.textContent = 'calendar is empty';
+   
  }
+ 
+ prevDate = chosenDate;
  
  for(let i=0; i<5; i++) {
  
@@ -134,11 +151,7 @@ const getChosenDate = () => {
 };
 
 
-
-
-/* ============= load data from the page loading ============ */
-
-
+/* ============= load data ============== */
 
 const getDatabase = async (chosenMonth) => {
  
@@ -149,16 +162,11 @@ const getDatabase = async (chosenMonth) => {
   return data;
 };
 
-getDatabase(chosenMonth)
+
+getDatabase(chosenMonth, chosenDate)
  .then((data) => {
 
  let m = data[0].scores[monthList[chosenMonth - 1]]; 
- 
- scores1 = m.sakko;
- scores2 = m.ujucha;
- scores3 = m.sujucha;
- scores4 = m.suwachochassa;
- scores5 = m.mudu;
  
  scoreList[0] = m.sakko;
  scoreList[1] = m.ujucha;
@@ -166,30 +174,41 @@ getDatabase(chosenMonth)
  scoreList[3] = m.suwachochassa;
  scoreList[4] = m.mudu;
  
- //test.textContent = scoreList[0];
- //test.textContent = "resolved! " + scores5;
+ for(let i=0; i<31; i++) {
+     if(chosenDate - 1 !== i) {
+     quality1[i].textContent = scoreList[0][i];
+     quality2[i].textContent = scoreList[1][i];
+     quality3[i].textContent = scoreList[2][i];
+     quality4[i].textContent = scoreList[3][i];
+     quality5[i].textContent = scoreList[4][i];
+     }
+     
+     if(chosenDate - 1 === i) {
+       quality1[i].querySelector('select').value = scoreList[0][i];
+       quality2[i].querySelector('select').value = scoreList[1][i];
+       quality3[i].querySelector('select').value = scoreList[2][i];
+       quality4[i].querySelector('select').value = scoreList[3][i];
+       quality5[i].querySelector('select').value = scoreList[4][i];
+     }
+   
+ }
+
 })
  .catch((err) => {
 
  test.textContent = "we got an error: " + err;
 });
 
-
-
 /* ==================== get input ============================= */
-
-
-
 
 
 quality1.forEach((q) => {
  
  q.addEventListener('change', (e) => {
    
-   let id = q.id; 
-   let index = (parseInt(id[id.length - 1])) - 1;
-   //scores1[index] = e.target.value;
-   //scoreList[0][index] = e.target.value;
+   /*let id = q.id; 
+   let index = (parseInt(id[id.length - 1])) - 1; */
+   let index =  chosenDate - 1;
    postList[0][index] = e.target.value;
   });
 });
@@ -199,10 +218,9 @@ quality2.forEach((q) => {
  
  q.addEventListener('change', (e) => {
    
-   let id = q.id; 
-   let index = (parseInt(id[id.length - 1])) - 1;
-   //scores2[index] = e.target.value;
-   //scoreList[1][index] = e.target.value;
+   /*let id = q.id; 
+   let index = (parseInt(id[id.length - 1])) - 1;*/
+   let index =  chosenDate - 1;
    postList[1][index] = e.target.value;
   });
 });
@@ -211,10 +229,9 @@ quality3.forEach((q) => {
  
  q.addEventListener('change', (e) => {
    
-   let id = q.id; 
-   let index = (parseInt(id[id.length - 1])) - 1;
-   //scores3[index] = e.target.value;
-   //scoreList[2][index] = e.target.value;
+  /* let id = q.id; 
+   let index = (parseInt(id[id.length - 1])) - 1;*/
+   let index =  chosenDate - 1;
    postList[2][index] = e.target.value;
   });
 });
@@ -224,10 +241,9 @@ quality4.forEach((q) => {
 
  q.addEventListener('change', (e) => {
    
-   let id = q.id; 
-   let index = (parseInt(id[id.length - 1])) - 1;
-   //scores4[index] = e.target.value;
-   //scoreList[3][index] = e.target.value;
+   /*let id = q.id;  
+   let index = (parseInt(id[id.length - 1])) - 1; */
+   let index =  chosenDate - 1;
    postList[3][index] = e.target.value;
   });
 });
@@ -236,43 +252,45 @@ quality4.forEach((q) => {
 quality5.forEach((q) => {
  
  q.addEventListener('change', (e) => {
-   
+   //q.id = 'q5-day' + chosenDate;
+   let index = chosenDate - 1;
+   /*
    let id = q.id; 
-   let index = (parseInt(id[id.length - 1])) - 1;
-   //scoreList[4][index] = e.target.value;   
-   postList[4][index] = e.target.value;   
+   let index = (parseInt(id[id.length - 1])) - 1; */ 
+   postList[4][index] = e.target.value; 
+   test.textContent = postList[4][index];
   });
 });
 
 
 /* ============= POST request & create charts ================ */
-
+/*
 const postRequest = () => {
 
  for(let i=0; i<5; i++) {
    for(let j=0; j<31; j++) {
-     if((Number.isNaN(postList[i][j])=== false) {
-        
+     if(isNaN(postList[i][j])=== false) {
+     
         fetch('https://raw.githubusercontent.com/MkDay/json_database/main/database.json', {
-          method: 'PUT',
+          method: 'POST',
           body: JSON.stringify({
-           year: 2022,
-           scores: {
-             monthList[chosenMonth - 1]: {
-               qualityList[i]: postList[i]
-             } 
-           }
-          //data[0].scores[monthList[chosenMonth - 1]] = postList[i][j]
+             year: 2022,
+             scores: {
+               monthList[chosenMonth - 1]: {
+                 qualityList[i]: postList[i]
+               }
+             }                      
+          //data[0].scores[monthList[chosenMonth - 1]] = postList[i][j];
           }),
           headers: { 
-            'Content-type': 'application/json' 
-            }
+            'Content-type': 'application/json; charset=UTF-8', 
+            },
                                       
         })
         .then((response) => response.json())
-        .then((json) => test.textContent = json //console.log(json))
+        .then((json) => test.textContent = json)
         .catch((err) => {
-          console.log('we have some error: ' + err.message);
+          test.textContent = 'we have some error: ' + err;
         });
      }
    }
@@ -280,12 +298,16 @@ const postRequest = () => {
 
 };
 
-
+*/
 
 btn.addEventListener('click', (e) => {
   
-  postRequest();
   
+  //postRequest();
+  
+  
+  container1.style.visibility = 'visible';
+  container2.style.visibility = 'visible';
   
   let sakko = document.querySelector('#sakko').getContext('2d');
   let ujucha = document.querySelector('#ujucha').getContext('2d');
@@ -294,6 +316,10 @@ btn.addEventListener('click', (e) => {
   let mudu = document.querySelector('#mudu').getContext('2d');
   
   let daily = document.querySelector('#daily');
+
+
+Chart.defaults.color = 'snow';
+Chart.defaults.font.size = 17;
 
 let sakkoChart = new Chart(sakko, {
 
@@ -370,6 +396,7 @@ let dailyChart = new Chart(daily, {
        label: 'Sakko',
        data: scoreList[0],
        backgroundColor: 'rgba(255, 99, 132, 0.6)'
+       
    },
    {
        label: 'Ujucha',
@@ -379,17 +406,20 @@ let dailyChart = new Chart(daily, {
    {
        label: 'Sujucha',
        data: scoreList[2],
-       backgroundColor: 'rgba(255, 206, 86, 0.6)' 
+       backgroundColor: 'rgba(255, 206, 86, 0.6)'
+       
    },
    {
        label: 'Suwachochassa',
        data: scoreList[3],
-       backgroundColor: 'rgba(75, 192, 192, 0.6)' 
+       backgroundColor: 'rgba(75, 192, 192, 0.6)'
+       
    },
    {
        label: 'Mudu',
        data: scoreList[4],
-       backgroundColor: 'rgba(153, 102, 255, 0.6)' 
+       backgroundColor: 'rgba(153, 102, 255, 0.6)'
+      
    }
    ]
  }, 
@@ -399,48 +429,10 @@ let dailyChart = new Chart(daily, {
 });
 
 
-
-
 /*
-file:///storage/emulated/0/project_folder/meditation_progress/database.json
-
-
-** json data structure:
-
-** get data from the database: (we have a problem with this)
-* fetch api is working.
-* json file looks fine (it's validated).
-* have tried database.json as the url.
-* have tried full url.
-* upload json data into github and tried to fetch data from there (this url only leads to the github page).
-
-https://github.com/MkDay/json_database/blob/main/meditation_database.json
-
-* as the next thing try these twos: 
- (1) find out about the error message that we've got.
- (2) also find out how to store our own data somewhere else and fetch from and post into there.
-
-
-●●●●●●●●●●●●●●
-(2) work on the calendar
-
-** insert data into arrays:
- scores1 = data[0].scores.jan.sakko
-
-** fetch data when the page is loading from the json file and insert them into proper places. (eg: Day1 is already completed and we can fetch data related Day1)
-               Day1 Day2
-sakko:          2
-ujucha:         4
-sujucha:        0
-suwachochassa:  5
-mudu:           4 
-
-** then we have to generate select options only for the days that haven't completed yet.
-
-** we can use a calandar to focus to the current day.
-
-** also if the user wants to change data of a previous day he can select that date so that the day will be focused and select options will be appeared.
-
-●●●●●●●●●●●●
-(3) add some cool design to it.
+* solve the problem with posting data.
+* add style to graphs.
+* test it.
 */
+
+
